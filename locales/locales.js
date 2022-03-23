64842f8203e5f5c936ce24df890554daeb29bab4
Berkin <17015319+darkao@users.noreply.github.com>
2022-03-23 23:22:13 +0300

    Add Turkish translation

--- locales/locales.js
+++ locales/locales.js
@@ -6,9 +6,13 @@ import ru_RU from "./ru_RU";
 import it_IT from "./it_IT";
 import sk_SK from "./sk_SK";
 import es_ES from "./es_ES";
+<<<<<<< HEAD
 import cs_CS from "./cs_CS";
 import tr_TR from "./tr_TR";
 import ja_JP from "./ja_JP";
+=======
+import tr_TR from "./tr_TR";
+>>>>>>> d9643d3 ( Add Turkish translation)
 
 const locales = {
   en_US,
@@ -19,9 +23,13 @@ const locales = {
   it_IT,
   sk_SK,
   es_ES,
+<<<<<<< HEAD
   cs_CS,
   tr_TR,
   ja_JP
+=======
+  tr_TR
+>>>>>>> d9643d3 ( Add Turkish translation)
 };
 
 export default locales;
